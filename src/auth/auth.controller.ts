04c6d76ada20dc7fd0d775e52905bb7c0c91b0d3
José Carlos de Freitas <josecfreitas@outlook.com.br>
2021-05-15 18:57:40 -0300

    auth controller delegating to auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+class AuthRequest {
+	email: string;
+	password: string;
+}
 
 @Controller('auth')
 export class AuthController {
+	constructor(private authService: AuthService) {}
+
 	@Post()
-	auth(): string {
-		return 'auth';
+	auth(@Body() authRequest: AuthRequest): string {
+		return this.authService.auth(authRequest.email, authRequest.password);
 	}
 
 	@Post('recover-password')
-	recoverPassword(): string {
-		return 'recover-password';
+	recoverPassword(@Body('email') email: string): string {
+		return this.authService.recoverPassword(email);
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	auth(email: string, password: string): string {
+		return `Trying to login => ${email}:${password}`;
+	}
+
+	recoverPassword(email: string): string {
+		return `Trying to recover password => ${email}`;
+	}
+}
